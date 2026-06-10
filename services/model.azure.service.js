49d3c00a42f50962acf45b9fffefc6d813b9c0d8
codealoalex <alexAvalosMS@outlook.com>
2026-06-10 13:13:15 -0400

    actualizando parametros

--- services/model.azure.service.js
+++ services/model.azure.service.js
@@ -9,8 +9,6 @@ export async function main(prompt) {
   const apiKey = process.env.AZURE_OPENAI_KEY;
   const apiVersion = "2024-04-01-preview";
   const options = { endpoint, apiKey, deployment, apiVersion };
-  /* const systemMessage =
-    "Eres el Asistente Virtual del SEGIP Bolivia. Tu función es informar sobre trámites de Cédula de Identidad y Licencias. Debes ser preciso con los requisitos y costos basados en los datos proporcionados. Si la información no está en el contexto, dirige al usuario a www.segip.gob.bo. No respondas con el texto tal cual está en los documentos, resumela y dale un formato legible, profesional y formal, no hagas uso de Markdown. La respuesta no debe superar las 150 palabras. No cites el documento del cual obtuviste la informacion. IMPORTANTE: No devuelvas información con texto en Markdown, omite asteriscos, asteriscos dobles, guiones, etc. Solo devuelve texto plano con formato legible, profesional y formal. TAMBIEN IMPORTANTE, en caso de que se te pregunte cuales son las oficinas más cercanas, pregunta donde se encuentra el usuario y si te proporciona la ciudad, brinda las centrales. Si preguntan por los horarios de oficina, analiza los archivos y determina cual es el horario de oficina más común y en este caso si no se encuentra asegura que esos son los horarios de oficina."; */
   const systemMessage = `# ROL Y OBJETIVO
 Eres el Asistente Virtual oficial del SEGIP Bolivia. Tu única función es brindar información precisa, formal y profesional sobre trámites de Cédula de Identidad y Licencias de Conducir en Bolivia, basándote estrictamente en los documentos provistos y las reglas del sistema.
 
@@ -36,7 +34,7 @@ Eres el Asistente Virtual oficial del SEGIP Bolivia. Tu única función es brind
         { role: "system", content: systemMessage },
         { role: "user", content: prompt },
       ],
-      max_tokens: 800,
+      max_tokens: 2000,
       temperature: 0.1,
       top_p: 0.5,
       model: modelName,
@@ -50,8 +48,8 @@ Eres el Asistente Virtual oficial del SEGIP Bolivia. Tu única función es brind
               type: "api_key",
               key: process.env.AZURE_SEARCH_KEY,
             },
-            top_n_documents: 12,
-            strictness: 2,
+            top_n_documents: 8,
+            strictness: 3,
           },
         },
       ],
